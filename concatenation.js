f5ba6c409ecaba96cf7198c92444ba0e3dcf0b9f
PresidentGanda <solomon.bennett55@gmail.com>
2025-06-16 14:32:14 -0400

    4.2

--- concatenation.js
+++ concatenation.js
@@ -1,19 +1,20 @@
 let submitButton = document.querySelector(".submit");
-let results = document.querySelector(".results");
-
+let result = document.querySelector(".result");
 submitButton.addEventListener("click", function() {
-    results.style.display = "block";
+    result.style.display = "block";
     let myName = document.querySelector(".my-name").value;
-    let mood = document.querySelector(".mood").value;
-    let food = document.querySelector(".food-name").value;
-    let birthYear = document.querySelector(".year").value;
-    let age = 2023 - birthYear; // update to 2024, if necessary
+    let moodElem = document.querySelector(".mood").value;
+    let foodElem = document.querySelector(".food-name").value;
+    let birthYearElem = document.querySelector(".year").value;
+    let age = 2025 - birthYearElem; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+let cityElem = document.querySelector(".city");
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
 
+let solresult = "My name is " + myName + " my mood is " + moodElem + " my fav food is " + foodElem + " Im " + age + " years old I live in " + cityElem;
+result.innerHTML = solresult;
 
 });
